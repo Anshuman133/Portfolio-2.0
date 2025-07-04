963469f1f5fb0e0350e431f0f3d18d0e27a653c5
Anshuman133 <paridaanshuman48@gmail.com>
2025-07-04 12:33:32 +0530

    Update Navbar NAd Work page components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Link, NavLink } from 'react-router-dom';
-import Hamburger from '../assets/hamburger.png'
-function handelClick(){
-  
-}
+import Hamburger from '../assets/hamburger.png';
+import Cross  from "../assets/cross.png"
+import { useState } from 'react';
+
 function Navbar()  {
+  const [nav, setNav] = useState(false);
   return (
     <div className=' flex justify-end text-gray-800 sm:px-24  pt-5 md:pt-8 md:mr-20'>
         <ul className=' hidden font-bold sm:flex justify-end  space-x-10  '>
@@ -29,13 +30,26 @@ function Navbar()  {
           </li>
 
         </ul>
-        <div className=' flex justify-end mr-6 '>
-          <img 
-            onClick={handelClick}
-            src={Hamburger} 
-            alt=""
+
+        <div onClick={() => setNav(!nav)}>
+           <div className=' flex justify-end mr-6 '>
+            <img 
+            src={nav ? Cross : Hamburger}
+            alt={nav ? "Close" : "Open"}
             className=' h-8 w-8 cursor-pointer sm:hidden' />
+           </div>
         </div>
+        {nav && (
+          <ul 
+           className='flex flex-col justify-center items-center absolute top-0 left-0
+                      w-full h-screen bg-cyan-100 text-gray-500 space-y-5 text-xl scroll-smooth '>
+            <Link to = "/"><li>Home</li></Link>
+            <Link to = "/works" ><li>Works</li></Link>
+            <Link to = "/blog" ><li>Blog</li></Link>
+            <Link to ="/contact" ><li>Contact</li></Link>
+            
+          </ul>
+        ) }
         
     </div>
   );
