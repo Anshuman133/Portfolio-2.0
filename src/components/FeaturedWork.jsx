a624e91642b9384ce971877d7f2cefa2172fa6c8
Anshuman133 <paridaanshuman48@gmail.com>
2025-07-04 12:38:32 +0530

    Update Navbar and Work page components

--- src/components/FeaturedWork.jsx
+++ src/components/FeaturedWork.jsx
@@ -5,48 +5,40 @@ import LiveDocs from "../assets/livedocs.avif"
 import Demo  from "../assets/pimg1.jpg"
 function FeaturedWorks () {
    const Works = [
-        {   
-            id : 0,
-            image : Portfolio,
-            heading : "Portfolio",
-            timing : "Live",
-            webType : "Code",
-            live : "",
-            code : "",
-            content : "A modern and responsive personal portfolio website to showcase your work, skills, and contact info. Designed with clean UI using React and Tailwind CSS, it's a professional space to present yourself as a developer."
-        },
-        { 
-            id :1,
-            image : Yoom,
-            heading : "Yoom ",
-            timing : "Live",
-            webType : "Code",
-            live : "",
-            code : "",
-            content : "Yoom is a full-stack video conferencing platform featuring meeting scheduling, video call reminders, and seamless real-time communication. Built with React, Tailwind CSS, Node.js, and MongoDB, and powered by Clerk for secure authentication. Fully responsive and optimized for a smooth user experience across devices."
-        },
-        {
-            id : 2,
-            image : LiveDocs,
-            heading : "LiveDocs",
-            timing : "Live",
-            webType : "Code",
-            live : "",
-            code : "",
-            content : "Live Docs is a real-time document collaboration tool, similar to Google Docs. It allows users to edit documents simultaneously, share links, and manage user sessions. Developed using React, Tailwind CSS, Node.js, and MongoDB, with Clerk integration for authentication. Fully responsive and ideal for productivity on the go."
-        },
-        {
-            id : 3,
-            image : Demo,
-            heading : "This a new hot topic",
-            timing : " Live",
-            webType : "Code",
-            live : "",
-            code : "",
-            content : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ab est aut officiis ea numquam inventore enim delectus quisquam magnam?"
-        }
-
-    ]
+              {   
+                  id : 0,
+                  image : Portfolio,
+                  heading : "Portfolio",
+                  live : "https://portfolio-2-0-iota-brown.vercel.app",
+                  code : "https://github.com/Anshuman133/Portfolio-2.0",
+                  content : "A modern and responsive personal portfolio website to showcase your work, skills, and contact info. Designed with clean UI using React and Tailwind CSS, it's a professional space to present yourself as a developer."
+              },
+              { 
+                  id :1,
+                  image : Yoom,
+                  heading : "Yoom ",
+                  live : "https://yoom.vercel.app",
+                  code : "https://github.com/Anshuman133/YOOM",
+                  content : "Yoom is a full-stack video conferencing platform featuring meeting scheduling, video call reminders, and seamless real-time communication. Built with React, Tailwind CSS, Node.js, and MongoDB, and powered by Clerk for secure authentication. Fully responsive and optimized for a smooth user experience across devices."
+              },
+              {
+                  id : 2,
+                  image : LiveDocs,
+                  heading : "LiveDocs",
+                  live : "https://jsm-live-docs.vercel.app",
+                  code : "https://github.com/Anshuman133/LiveDocs",
+                  content : "Live Docs is a real-time document collaboration tool, similar to Google Docs. It allows users to edit documents simultaneously, share links, and manage user sessions. Developed using React, Tailwind CSS, Node.js, and MongoDB, with Clerk integration for authentication. Fully responsive and ideal for productivity on the go."
+              },
+              {
+                  id : 3,
+                  image : Demo,
+                  heading : "The Blog",
+                  live : "",
+                  code : "",
+                  content : "The Blog is a clean, user-friendly blogging platform for writers and readers. Create, edit, and publish posts with ease. Enjoy features like user profiles, comments, categories, and a responsive design. Whether sharing ideas or stories, The Blog offers a simple, elegant space to express yourself and connect with others."
+              }
+      
+          ]
   return(
     <div className="mx-5 sm:mx-24 md:mx-44 lg:mx-56 ">
         <div className=" flex justify-between pt-5">
@@ -67,10 +59,10 @@ function FeaturedWorks () {
                     <div className="space-y-2">
                      <h1 className="text-2xl font-bold">{heading}</h1>
                     <div className="flex items-center gap-4">
-                       <span className="bg-gray-800 text-white text-sm font-semibold rounded-full px-3 py-0.5">
-                            {timing}
-                     </span>
-                     <span className="text-gray-500">{webType}</span>
+                       <Link to ={live} ><span className="bg-gray-800 text-white text-sm font-semibold rounded-full px-3 py-0.5">
+                            Live
+                     </span></Link>
+                    <Link to = {code}> <span className="text-gray-500">Code</span> </Link>
                    </div>
                      <p className="text-gray-700">{content}</p>
                      
